214c06a8d3f1b8f08f37b1823270b153a5e9d783
James Lamb <jaylamb20@gmail.com>
2022-10-09 21:24:03 -0500

    add more docker cleanup stuff

--- bash_profile/docker.sh
+++ bash_profile/docker.sh
@@ -5,13 +5,18 @@
 # [description] remove ALL local docker containers
 # [usage] nuke_all_dockers
 # [ref] https://github.com/docker/machine/issues/1779
+# [ref] https://docs.docker.com/desktop/faqs/macfaqs/
 nuke_all_dockers() {
-	# kill running containers
-	docker kill $(docker ps -qa)
-	# delete them
-	docker rm -v $(docker ps -qa)
-	# remove all images
-	docker rmi -f $(docker images -q)
+    # kill running containers
+    docker kill $(docker ps -qa)
+    # delete them
+    docker rm -v $(docker ps -qa)
+    # remove all images
+    docker rmi -f $(docker images -q)
+    # remove all volumes
+    docker volume rm $(docker volume ls -q)
+    # remove everything
+    docker system prune --all -f
 }
 
 # [description] kill the most recent container you started
